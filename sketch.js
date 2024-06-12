9cdc3b20661d7ce5a35f0b7c9cb270a867d46a06
MariaInsanidadeTotal69 <00001119494916sp@al.educacao.sp.gov.br>
2024-06-12 16:28:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-  }
+  createCanvas(600,600);
+   background("white");
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+  if (mouseIsPressed) {
+  rect(mouseX,mouseY, 20,20);
+}
+}
